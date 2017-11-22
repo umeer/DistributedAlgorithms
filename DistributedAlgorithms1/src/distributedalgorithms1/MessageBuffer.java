e5cd3315c798d61613de6cb1534aee1c021b372a
Umeer Mohammad <umeermohammad@gmail.com>
2017-11-22 20:59:59 +0100

    -Test implemented

---
+++ DistributedAlgorithms1/src/distributedalgorithms1/MessageBuffer.java
@@ -0,0 +1,13 @@
+package distributedalgorithms1;
+
+public class MessageBuffer {
+
+    public String message;
+    public long timestamp;
+
+   public MessageBuffer(String message, long timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+}
