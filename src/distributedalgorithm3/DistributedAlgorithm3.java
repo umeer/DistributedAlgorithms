12da22fccb4db3c8917129edbcae8d47a39dbe16
Umeer Mohammad <umeermohammad@gmail.com>
2017-12-18 20:17:58 +0100

    -Creation of new project

--- src/distributedalgorithm3/DistributedAlgorithm3.java
+++ src/distributedalgorithm3/DistributedAlgorithm3.java
@@ -11,9 +11,7 @@ package distributedalgorithm3;
  */
 public class DistributedAlgorithm3 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
